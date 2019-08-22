994706ab4ce6d2ba71abf4afbeb0d70a7a5e693c
stefan521 <muscalu.stefan@yahoo.com>
2019-08-22 21:04:22 +0100

    Move components in separate files.

---
+++ src/Board.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Square from './Square.js';
+
+export default class Board extends React.Component {
+  renderSquare(squareNumber) {
+    return (
+       <Square
+          value={this.props.squares[squareNumber]}
+          onClick={() => this.props.onClick(squareNumber)}
+          key={squareNumber}
+       />
+    );
+  }
+
+  render() {
+    let rows = [];
+    let boardSize = 3;
+
+    for(let i = 0; i < boardSize; i++) {
+      let row = [];
+      for(let j = 0; j < boardSize; j++) {
+        let squareNumber = i*boardSize + j;
+        row.push(this.renderSquare(squareNumber));
+      }
+      rows.push(<div className="board-row" key={i}>{row}</div>);
+    }
+
+    return (<div>{rows}</div>);
+  }
+}
