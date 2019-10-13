7ba7b7eb01e9d0d3f3849f6bc94c5df74233c735
stefan521 <muscalu.stefan@yahoo.com>
2019-10-13 18:42:14 +0100

    Updates

---
+++ precache-manifest.27996c54ede5017a6557ac3a236d345f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683b4b7286d03047ebde28f0537ff52b",
+    "url": "/TicTacToe-React/index.html"
+  },
+  {
+    "revision": "2d39abc06776b4295b0a",
+    "url": "/TicTacToe-React/static/css/main.237a2a18.chunk.css"
+  },
+  {
+    "revision": "653d90e5c7e15762d588",
+    "url": "/TicTacToe-React/static/js/2.7716867a.chunk.js"
+  },
+  {
+    "revision": "2d39abc06776b4295b0a",
+    "url": "/TicTacToe-React/static/js/main.3c627a37.chunk.js"
+  },
+  {
+    "revision": "bd1ca0e02c188e24c807",
+    "url": "/TicTacToe-React/static/js/runtime~main.39cad35b.js"
+  }
+]);
